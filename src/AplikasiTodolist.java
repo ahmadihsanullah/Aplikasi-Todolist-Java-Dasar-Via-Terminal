9f4e134fdccca9f8ee7f0cc0717cccbd2e9e0c87
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2023-06-26 22:18:43 +0700

    add todolist: java dasar

--- src/AplikasiTodolist.java
+++ src/AplikasiTodolist.java
@@ -6,7 +6,7 @@ public class AplikasiTodolist {
      */
     public static String[] model = new String[10];
     public static void main(String[] args) {
-       viewShowTodolist();
+       testRemoveTodolist();
     }
 
     //BUSINESS LOGIC
