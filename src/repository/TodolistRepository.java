585eda8b274b5e11773d4a65214d59b2db67dce3
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2023-06-26 23:15:03 +0700

    add todolist repository

--- src/repository/TodolistRepository.java
+++ src/repository/TodolistRepository.java
@@ -1,2 +1,11 @@
-package repository;public interface TodolistRepository {
+package repository;
+
+import entity.Todolist;
+
+public interface TodolistRepository {
+    Todolist[] getAll();
+
+    void add(Todolist todolist);
+
+    void remove(Integer number);
 }
--- src/repository/TodolistRepositoryImpl.java
+++ src/repository/TodolistRepositoryImpl.java
@@ -1,2 +1,21 @@
-package repository;public class TodolistRepositoryImpl {
+package repository;
+
+import entity.Todolist;
+
+public class TodolistRepositoryImpl implements TodolistRepository{
+    public Todolist[] data = new Todolist[10];
+    @Override
+    public Todolist[] getAll() {
+        return data;
+    }
+
+    @Override
+    public void add(Todolist todolist) {
+
+    }
+
+    @Override
+    public void remove(Integer number) {
+
+    }
 }
