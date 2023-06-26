1365578d587cdc095d0d7c8cd6f719c15cd98d8c
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2023-06-26 23:15:19 +0700

    add todolist service

--- src/service/TodolistServiceImpl.java
+++ src/service/TodolistServiceImpl.java
@@ -1,2 +1,37 @@
-package service;public class TodolistServiceImpl {
+package service;
+
+import entity.Todolist;
+import repository.TodolistRepository;
+
+public class TodolistServiceImpl implements TodolistService{
+
+    private TodolistRepository todoListRepository;
+
+    public TodolistServiceImpl(TodolistRepository todoListRepository) {
+        this.todoListRepository = todoListRepository;
+    }
+    @Override
+    public void showTodoList() {
+        Todolist[] model = todoListRepository.getAll();
+
+        System.out.println("TODOLIST");
+        for (var i = 0; i < model.length ; i ++){
+            var todolist = model[i];
+            var no = i + 1;
+
+            if(todolist != null){
+                System.out.println(no + ". " + todolist.getTodo());
+            }
+        }
+    }
+
+    @Override
+    public void addTodoList(String todolist) {
+
+    }
+
+    @Override
+    public void removeTodoList(Integer number) {
+
+    }
 }
