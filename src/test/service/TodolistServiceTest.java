4ab298dfe903104b9fe7d02c9791d17e21d35a17
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2023-06-26 23:15:53 +0700

    add todolist service test

--- src/test/service/TodolistServiceTest.java
+++ src/test/service/TodolistServiceTest.java
@@ -1,2 +1,24 @@
-package test.service;public class TodolistServiceTest {
+package test.service;
+
+import entity.Todolist;
+import repository.TodolistRepository;
+import repository.TodolistRepositoryImpl;
+import service.TodolistService;
+import service.TodolistServiceImpl;
+
+public class TodolistServiceTest {
+    public static void main(String[] args) {
+        testShowTodolist();
+    }
+
+    public static void testShowTodolist()
+    {
+        TodolistRepositoryImpl todolistRepository = new TodolistRepositoryImpl();
+        todolistRepository.data[0] = new Todolist("Belajar Java Dasar");
+        todolistRepository.data[1] = new Todolist("Belajar Java OOP");
+        todolistRepository.data[2] = new Todolist("Belajar Java Standard Classess");
+        TodolistService todolistService = new TodolistServiceImpl(todolistRepository);
+
+        todolistService.showTodoList();
+    }
 }
